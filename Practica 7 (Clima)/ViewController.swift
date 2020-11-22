5c385ebe697ec778e8aedde65993c38b5dc22743
Ingrid <ingrid@Ingrids-MBP.lan>
2020-11-21 19:40:36 -0600

    Proyecto creado

---
+++ Practica 7 (Clima)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Practica 7 (Clima)
+//
+//  Created by Ingrid on 21/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
